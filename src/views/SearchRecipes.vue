03c5d52dbde3882ddaea8b2b3f0c268ba8d62778
Igor Solodovnick <info@pro-landing.site>
2021-11-22 17:55:47 +0300

    стилизовал вывод информации в карточках

--- src/views/SearchRecipes.vue
+++ src/views/SearchRecipes.vue
@@ -20,46 +20,81 @@
             </div>
             <div class="recipe-card__description">
               <div class="recipe-card__ingredients">
-                <h3>Ингредиенты</h3>
-                <ul class="recipe-card__ingredients-list">
-                  <li
-                    class="recipe-card__ingredients-item"
-                    v-for="ingredient in item.ingredientLines"
-                    :key="ingredient.id"
-                  >
-                    {{ ingredient }}
-                  </li>
-                </ul>
+                <el-collapse accordion>
+                  <el-collapse-item title="Ингредиенты" :name="index">
+                    <div
+                      class="recipe-card__ingredients-item"
+                      v-for="(ingredientLine, ingredientIndex) in item.ingredientLines"
+                      :key="ingredientIndex"
+                    >
+                      <img
+                        class="recipe-card__ingredients-logo"
+                        :src="item.ingredients[ingredientIndex].image"
+                      />
+                      <span class="recipe-card__ingredients-text">
+                        {{ ingredientLine }}
+                      </span>
+                    </div>
+                  </el-collapse-item>
+
+                  <el-collapse-item title="Характеристики" :name="item.calories">
+                    <p>{{ Math.round(item.calories) }} кал.</p>
+                    <p>Описание</p>
+                  </el-collapse-item>
+
+                  <el-collapse-item title="Предостережения" :name="item.cautions.index">
+                    <el-tag type="warning" v-if="!item.cautions.length">
+                      {{ 'Предостережений нет' }}
+                    </el-tag>
+                    <div v-else>
+                      <el-tag
+                        type="warning"
+                        v-for="(caution, cautionIndex) in item.cautions"
+                        :key="cautionIndex"
+                        style="margin-right: 10px"
+                        >{{ caution }}</el-tag
+                      >
+                    </div>
+                  </el-collapse-item>
+
+                  <el-collapse-item title="Тип кухни" :name="item.cuisineType.index">
+                    <el-tag v-if="!item.cuisineType.length"> {{ 'Нет информации' }}</el-tag>
+                    <div v-else>
+                      <el-tag v-for="cuisine in item.cuisineType" :key="cuisine.id">
+                        {{ cuisine }}</el-tag
+                      >
+                    </div>
+                  </el-collapse-item>
+
+                  <div v-if="item.dishType">
+                  <el-collapse-item title="Тип блюда" :name="item.dishType.index">
+                     <el-tag v-for="dish in item.dishType" :key="dish.id"> {{ dish }}</el-tag>
+                  </el-collapse-item>
+                  </div>
+
+                  <el-collapse-item title="Этикетки здоровья" :name="item.healthLabels.index">
+                    <el-tag
+                      type="success"
+                      style="margin-right: 10px; margin-bottom: 10px"
+                      v-for="healthLabel in item.healthLabels"
+                      :key="healthLabel.id"
+                    >
+                      {{ healthLabel }}</el-tag
+                    >
+                  </el-collapse-item>
+
+                  <el-collapse-item title="Тип еды" :name="item.mealType.index">
+                  <el-tag v-if="!item.mealType.length"> {{ 'Нет информации' }}</el-tag>
+                  <div v-else>
+                    <el-tag v-for="meal in item.mealType" :key="meal.id"> {{ meal }}</el-tag>
+                  </div>
+                  </el-collapse-item>
+                </el-collapse>
               </div>
-              <hr />
-              <h3>Характеристики</h3>
-              <p>{{ Math.round(item.calories) }} кал.</p>
-              <p>Описание</p>
-              <hr />
-              <h3>Предостережения</h3>
-              <el-tag
-                type="warning"
-                v-for="caution in item.cautions"
-                :key="caution.id"
-                style="margin-right: 10px"
-                >{{ caution }}</el-tag
-              >
-              <hr />
-              <h3>Тип кухни</h3>
-              <el-tag v-for="cuisine in item.cuisineType" :key="cuisine.id"> {{ cuisine }}</el-tag>
-              <hr />
-              <h3>Тип блюда</h3>
-              <el-tag v-for="dish in item.dishType" :key="dish.id"> {{ dish }}</el-tag>
-              <hr />
-              <h3>Этикетки здоровья</h3>
-              <el-tag
-                type="success"
-                style="margin-right: 10px; margin-bottom: 10px"
-                v-for="healthLabel in item.healthLabels"
-                :key="healthLabel.id"
-              >
-                {{ healthLabel }}</el-tag
-              >
+
+              <p class="search-recipes__source">Источник: {{ `«${item.source}»` }}</p>
+              <el-link class="search-recipes__to-recipe"
+              :href="item.url" target="_blank">Перейти к рецепту</el-link>
             </div>
           </div>
         </el-col>
@@ -126,12 +161,15 @@ export default {
     }
 
     &__ingredients {
-      &-list {
-        padding-left: 20px;
-        margin: 10px 0;
-      }
       &-item {
-        margin-bottom: 5px;
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      &-logo {
+        width: 30px;
+        border-radius: 50%;
+        margin-right: 10px;
       }
     }
 
@@ -143,8 +181,44 @@ export default {
   .el-row {
     margin-bottom: 10px;
   }
-  h3 {
-    margin-bottom: 10px;
+
+  &__source{
+    color: $color_7;
+    font-size: 14px;
+    margin: 10px 0;
   }
+
+  &__to-recipe{
+  @extend %baseButton;
+  font-size: 18px;
+  min-width: 250px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  }
+
+}
+</style>
+
+<style lang="scss">
+.el-collapse-item__header {
+  background-color: transparent;
+  color: white;
+}
+
+.el-collapse-item__wrap {
+  background-color: transparent;
+}
+
+.recipe-card__ingredients-item {
+  color: white;
+}
+
+.el-collapse-item__content{
+  padding-bottom: 15px;
+}
+
+.el-link.is-underline:hover:after {
+border: none;
 }
 </style>
